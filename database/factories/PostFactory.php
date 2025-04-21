2b4ed743891c14cf2d81ab120b19498da4e15b21
shota <takahashi_shota@mads.co.jp>
2025-04-21 19:39:04 +0900

    phase2: ページネーション機能実装

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str; // 必要に応じて使う
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,16 +16,9 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
-        // Fakerを使ってダミーデータを定義
         return [
-        // 日本語のダミーデータが生成されるようになる
-        'title' => $this->faker->realText(10), // 日本語のリアルなテキスト（最大50文字）
-        'content' => $this->faker->realText(200), // 日本語のリアルなテキスト（最大500文字）
-
-        // 他にも日本語対応のメソッドがあります
-        // 'author_name' => $this->faker->name(), // 日本語氏名
-        // 'address' => $this->faker->address(), // 日本語住所
-
+            'title' => $this->faker->realText(10),
+            'content' => $this->faker->realText(200),
         ];
     }
 }
