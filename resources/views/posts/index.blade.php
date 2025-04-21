d4ab5046e473ac89ff844a48e63e6ee427687619
shota <takahashi_shota@mads.co.jp>
2025-04-21 19:04:58 +0900

    phase1:一覧画面実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,27 @@
-@foreach($posts as $post)
-    <div>title : {{ $post->title }}</div>
-    <div>content : {{ $post->content }}</div>
-@endforeach
+@extends('default')
+
+@section('title', '投稿一覧')
+
+@section('content')
+    <h1>投稿一覧</h1>
+
+    <div class="container mt-4">
+        <div class="row">
+            @foreach ($posts as $post)
+                <div class="col-md-4 col-sm-6 col-12 mb-4">
+                    <div class="card h-100">
+
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+
+                            <p class="card-text">{{ \Illuminate\Support\Str::limit($post->content, 100) }}</p>
+                        </div>
+                        <div class="card-footer text-end">
+                            <small class="text-muted">投稿日: {{ $post->created_at->format('Y-m-d H:i') }}</small>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
