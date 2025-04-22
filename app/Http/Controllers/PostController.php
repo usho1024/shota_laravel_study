e2b9771ef162b2cb5470fd8587e00bcc1988a89c
shota <takahashi_shota@mads.co.jp>
2025-04-22 12:43:34 +0900

    phase3: 新規投稿処理実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -15,10 +16,42 @@ class PostController extends Controller
      */
     public function index(): View
     {
-        $posts = Post::query()->paginate(20);
+        $posts = Post::query()
+            ->orderByDesc('created_at')
+            ->paginate(20);
 
         return view('posts.index', [
             'posts' => $posts
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create'); // 新規投稿フォームのビューを返す
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        // shim : インスタンス化の文法が間違っています
+        // $post = new Post(); のように（）が必要です
+        $post = new Post();
+
+        // $requestからinput情報を取得するときはinput()を使いましょう
+        // $request->input('title');
+        // なくても動作はされますが、これが$requestのプロパティなのか？ユーザinputなのか判断できないので！
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+
+        // この形だとPostのプロパティのsaveというのを取得するだけの処理になります
+        // 僕達は保存をしたいので保存の役割を果たすメソッドを呼び出す必要があります
+        $post->save();
+
+        // まずは、returnをしてないです。このstore()アクションはRedirectResponseを返さないといけないとPHPDocに書いてあります。
+        // リダイレクトのやり方はドキュメントを読んでまた調べてみてください
+        return redirect()->route('posts.index');
     }
 }
