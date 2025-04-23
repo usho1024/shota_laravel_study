ec37daa5e199ea5230795156c76c8d3ea2a62c4b
shota <takahashi_shota@mads.co.jp>
2025-04-23 19:01:33 +0900

    phase7: 投稿の復元機能を実装途中

--- resources/views/components/modal/restore.blade.php
+++ resources/views/components/modal/restore.blade.php
@@ -1,14 +1,14 @@
-<div class="modal fade" id="deleteModal{{ $model->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $model->id }}" aria-hidden="true">
+<div class="modal fade" id="restoreModal{{ $model->id }}" tabindex="-1" aria-labelledby="restoreModalLabel{{ $model->id }}" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
-                この投稿（ID: {{ $model->id }}）を本当に削除しますか？
+                この投稿（ID: {{ $model->id }}）を復元しますか？
             </div>
             <div class="modal-footer text-center">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">いいえ</button>
-                <form action="{{ route("$table_name.destroy", $model->id) }}" method="POST">
+                <form action="{{ route("$table_name.restore", $model->id) }}" method="POST">
                     @csrf
-                    @method('DELETE')
+                    @method('PUT')
                     <button type="submit" class="btn btn-primary">はい</button>
                 </form>
             </div>
