29ed98485b115e4817c7709803f6155fcd0a4b90
shota <takahashi_shota@mads.co.jp>
2025-04-24 16:26:57 +0900

    phase9: 認証機能を実装

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,30 @@
+@extends('default')
+
+@section('title', 'ログイン')
+
+@section('content')
+    <div class="container mt-4">
+        <h1 class="mb-4">ログイン</h1>
+
+        @error('unmatch')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">email</label>
+                <input type="mail" id="email" name="email" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">password</label>
+                <input type="password" id="password" name="password" class="form-control">
+            </div>
+
+            <div class="d-grid mb-3">
+                <button type="submit" class="btn btn-primary">ログイン</button>
+            </div>
+        </form>
+    </div>
+@endsection
