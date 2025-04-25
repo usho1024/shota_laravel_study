7b5a8c8b404694cc0be7f26d38bda8cc892cc5ff
ToBeSD <kuzoteki@gmail.com>
2025-04-25 10:57:14 +0900

    Laravel Documentに沿ってRouteのUriを再定義した

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/posts/update/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/destroy/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::put('/posts/restore/{post}', [PostController::class, 'restore'])->name('posts.restore');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::post('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::put('/posts/{post}', [PostController::class, 'restore'])->name('posts.restore');
