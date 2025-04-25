ecf3ff72b453f1528ecce40ff83bca8f8c593957
ToBeSD <kuzoteki@gmail.com>
2025-04-25 16:36:15 +0900

    一緒にGateが使えない不具合を解決した

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'index'])->middleware(RedirectIfAuthenticated::class)->name('index');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::middleware(['can:manage-post'])->group(function () {
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::post('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-    Route::put('/posts/{post}/restore', [PostController::class, 'restore'])->name('posts.restore');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::middleware(['can:manage-post,post'])->group(function () {
+        Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+        Route::post('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+        Route::put('/posts/{post}/restore', [PostController::class, 'restore'])->name('posts.restore');
+    });
 });
